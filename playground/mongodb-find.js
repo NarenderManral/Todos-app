e54a9bdcf134f8834a490c520224e987220bae3d
Narender <manralnabbu@gmail.com>
2020-04-22 13:11:33 +0530

    Todo app created

---
+++ playground/mongodb-find.js
@@ -0,0 +1,31 @@
+const {MongoClient,ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').find({_id:new ObjectID('5e9f35015e67a406e441bf9f')}).toArray().then((docs)=>{
+  //   console.log("Todos");
+  //   console.log(JSON.stringify(docs,undefined,2));
+  // },(err)=>{
+  //     console.log("Unable to fetch todos",err);
+  // });
+
+  // db.collection('Todos').find().count().then((count)=>{
+  //   console.log('Todos count:'+count);
+  //   },(err)=>{
+  //     console.log("Unable to fetch todos",err);
+  // });
+
+  db.collection('Users').find({name:'Andrew'}).toArray().then((docs)=>{
+    console.log('Users :');
+    console.log(JSON.stringify(docs,undefined,2));
+    },(err)=>{
+      console.log("Unable to fetch todos",err);
+  });
+  
+
+  db.close();
+});
