1d4a6d1b79ea62713e5ecf4e0f8647f71d313baa
Narender <manralnabbu@gmail.com>
2020-04-25 02:46:02 +0530

    patch update added

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ =require("lodash");
 var {mongoose}=require("./db/mongoose");
 var {Todo}=require("./models/todo");
 var {User} =require("./models/user");
@@ -62,6 +63,32 @@ app.delete("/todos/:id",(req,res)=>{
         res.status(400).send();
     });
 });
+
+app.patch("/todos/:id",(req,res)=>{
+    var id=req.params.id;
+    var body=_.pick(req.body,['text','completed']);
+
+    if(!ObjectId.isValid(id)){
+        return res.status(404).send();
+    }
+    if(_.isBoolean(body.completed)&& body.completed){
+        body.completedAt= new Date().getTime();
+    }
+    else{
+        body.completed=false;
+        body.completedAt=null;
+    }
+
+    Todo.findByIdAndUpdate(id,{$set :body},{new:true}).then((todo)=>{
+        if(!todo){
+            return res.status(404).send()
+        }
+        res.send({todo});
+    }).catch((e)=>{
+        res.status(400).send();
+    })
+});
+
 
 app.listen(port,()=>{
     console.log("Started on port :",port);
